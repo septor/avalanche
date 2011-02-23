8674065d41138d67aa57e11291d33c61daf14e3e
septor <patrickweaver@gmail.com>
2011-02-23 12:17:26 -0500

    debug commit

---
+++ admin_config.php
@@ -0,0 +1,102 @@
+<?php
+
+if(!defined("e107_INIT")) {
+	require_once("../../class2.php");
+}
+if(!getperms("P")){ header("location:".e_BASE."index.php"); exit;}
+require_once(e_ADMIN."auth.php");
+require_once(e_HANDLER."userclass_class.php");
+
+	
+if(isset($_POST['updatesettings'])){
+	$pref['wowapp_guildname'] = $_POST['wowapp_guildname'];
+	$pref['wowapp_rules'] = $_POST['wowapp_rules'];
+	$pref['wowapp_rulesrequired'] = $_POST['wowapp_rulesrequired'];
+	$pref['wowapp_viewclass'] = $_POST['wowapp_viewclass'];
+	$pref['wowapp_rankclass'] = $_POST['wowapp_rankclass'];
+	$pref['wowapp_manageclass'] = $_POST['wowapp_manageclass'];
+	$pref['wowapp_replymethod'] = $_POST['wowapp_replymethod'];
+	$pref['wowapp_externalallowed'] = $_POST['wowapp_externalallowed'];
+	$pref['wowapp_wowrecruitlink'] = $_POST['wowapp_wowrecruit'];
+	save_prefs();
+	$message = "Settings saved successfully!";
+}
+
+if (isset($message)) {
+	$ns->tablerender("", "<div style='text-align:center'><b>".$message."</b></div>");
+}
+
+$text = "
+<div style='text-align:center'>
+<form method='post' action='".e_SELF."'>
+<table style='width:75%' class='fborder'>
+<tr>
+<td style='width:50%' class='forumheader3'>What is your guild's name?</td>
+<td style='width:50%; text-align:right' class='forumheader3'>
+<input type='text' name='wowapp_guildname' class='tbox' value='".$pref['wowapp_guildname']."' />
+</td>
+</tr>
+<tr>
+<td style='width:50%' class='forumheader3'>Who can view applications?</td>
+<td style='width:50%; text-align:right' class='forumheader3'>
+".r_userclass('wowapp_viewaccess', $pref['wowapp_viewaccess'], 'off', 'nobody,member,admin,classes')."
+</td>
+</tr>
+<tr>
+<td style='width:50%' class='forumheader3'>Who can rank and comment on applications?</td>
+<td style='width:50%; text-align:right' class='forumheader3'>
+".r_userclass('wowapp_rankaccess', $pref['wowapp_rankaccess'], 'off', 'nobody,member,admin,classes')."
+</td>
+</tr>
+<tr>
+<td style='width:50%' class='forumheader3'>Who can manage applications?</td>
+<td style='width:50%; text-align:right' class='forumheader3'>
+".r_userclass('wowapp_manageaccess', $pref['wowapp_manageaccess'], 'off', 'nobody,member,admin,classes')."
+</td>
+</tr>
+<tr>
+<td style='width:50%' class='forumheader3'>Reply method used when contact applicants regarding their acceptance or denial:</td>
+<td style='width:50%; text-align:right' class='forumheader3'>
+<select name='wowapp_replymethod' class='tbox'>
+<option value='pm'".($pref['wowapp_replymethod'] == "pm" ? " selected" : "").">PM</option>
+<option value='email'".($pref['wowapp_replymethod'] == "email" ? " selected" : "").">Email</option>
+</select>
+</td>
+</tr>
+<tr>
+<td style='width:50%' class='forumheader3'>Allow external realm applicants?</td>
+<td style='width:50%; text-align:right' class='forumheader3'>
+<input type='checkbox' name='wowapp_externalallowed' value='1'".($pref['wowapp_externalallowed'] == 1 ? " checked='checked'" : "")." />
+</td>
+</tr>
+<tr>
+<td style='width:50%' class='forumheader3'>Link with the WoW Recruitment Menu?</td>
+<td style='width:50%; text-align:right' class='forumheader3'>
+<input type='checkbox' name='wowapp_wowrecruit' value='1'".($pref['wowapp_wowrecruitlink'] == 1 ? " checked='checked'" : "")." />
+</td>
+</tr>
+<tr>
+<td style='width:50%' class='forumheader3'>Require applicants to agree to your guild rules before their application is submitted?</td>
+<td style='width:50%; text-align:right' class='forumheader3'>
+<input type='checkbox' name='wowapp_rulesrequired' value='1'".($pref['wowapp_rulesrequired'] == 1 ? " checked='checked'" : "")." />
+</td>
+</tr>
+<tr>
+<td style='width:50%' class='forumheader3'>Guild Rules:</td>
+<td style='width:50%; text-align:right' class='forumheader3'>
+<textarea class='tbox' name='wowapp_rules' style='width:90%; height:50px;'>".$pref['wowapp_rules']."</textarea>
+</td>
+</tr>
+<tr>
+<td colspan='2' style='text-align:center' class='forumheader'>
+<input class='button' type='submit' name='updatesettings' value='Save Settings' />
+</td>
+</tr>
+</table>
+</form>
+</div>
+";
+
+$ns->tablerender("Configure WoW Guild Application", $text);
+require_once(e_ADMIN."footer.php");
+?>
---
+++ plugin.php
@@ -0,0 +1,110 @@
+<?php
+
+//PLUGIN INFO------------------------------------------------------------------------------------------------+
+
+$eplug_name        = "WoW Guild Application";
+$eplug_version     = "ALPHA.1";
+$eplug_author      = "septor";
+$eplug_url         = "http://painswitch.com/";
+$eplug_email       = "patrickweaver@gmail.com";
+$eplug_description = "World of Warcraft Guild Application";
+$eplug_compatible  = "e107 0.7+";
+$eplug_readme      = "";
+$eplug_compliant   = TRUE;
+$eplug_module      = FALSE;
+
+//PLUGIN FOLDER----------------------------------------------------------------------------------------------+
+
+$eplug_folder     = "wowapp";
+
+//PLUGIN MENU FILE-------------------------------------------------------------------------------------------+
+
+$eplug_menu_name  = "";
+
+//PLUGIN ADMIN AREA FILE-------------------------------------------------------------------------------------+
+
+$eplug_conffile   = "admin_config.php";
+
+//PLUGIN ICONS AND CAPTION-----------------------------------------------------------------------------------+
+
+$eplug_logo       = "";
+$eplug_icon       = "";
+$eplug_icon_small = "";
+$eplug_caption    = "Configure WoW Guild Application";
+
+//DEFAULT PREFERENCES----------------------------------------------------------------------------------------+
+
+$eplug_prefs = array(
+	"wowapp_guildname" => "",
+	"wowapp_rules" => "",
+	"wowapp_rulesrequired" => "1",
+	"wowapp_viewaccess" => "",
+	"wowapp_rankaccess" => "",
+	"wowapp_manageaccess" => "",
+	"wowapp_externalallowed" => "1",
+	"wowapp_replymethod" => "pm",
+	"wowapp_wowrecruitlink" => "0"
+);
+
+//MYSQL TABLES TO BE CREATED---------------------------------------------------------------------------------+
+
+$eplug_table_names = array("wowapp_application", "wowapp_request", "wowapp_comment", "wowapp_armory");
+
+//MYSQL TABLE STRUCTURE--------------------------------------------------------------------------------------+
+
+$eplug_tables = array(
+	"CREATE TABLE ".MPREFIX."wowapp_application (
+		wa_id int(10) unsigned NOT NULL auto_increment,
+		wa_key varchar(250) NOT NULL,
+		wa_fieldname varchar(250) NOT NULL,
+		wa_type varchar(250) NOT NULL,
+		wa_value text NOT NULL,
+		PRIMARY KEY  (wa_id)
+	) TYPE=MyISAM AUTO_INCREMENT=1;",
+
+	"CREATE TABLE ".MPREFIX."wowapp_request (
+		wa_id int(10) unsigned NOT NULL auto_increment,
+		wa_uid int(10) unsigned NOT NULL,
+		wa_qid varchar(250) NOT NULL,
+		wa_value text NOT NULL,
+		PRIMARY KEY  (wa_id)
+	) TYPE=MyISAM AUTO_INCREMENT=1;",
+
+	"CREATE TABLE ".MPREFIX."wowapp_comment (
+		wa_id int(10) unsigned NOT NULL auto_increment,
+		wa_uid int(10) unsigned NOT NULL,
+		wa_aid int(10) unsigned NOT NULL,
+		wa_comment text NOT NULL,
+		wa_vote int(10) unsigned NOT NULL default '2',
+		PRIMARY KEY  (wa_id)
+	) TYPE=MyISAM AUTO_INCREMENT=1;",
+
+	"CREATE TABLE ".MPREFIX."wowapp_armory (
+		wa_id int(10) unsigned NOT NULL auto_increment,
+		wa_class varchar(250) NOT NULL,
+		wa_spec varchar(250) NOT NULL,
+		wa_gather varchar(250) NOT NULL,
+		PRIMARY KEY  (wa_id)
+	) TYPE=MyISAM AUTO_INCREMENT=1;"
+);
+
+//LINK TO BE CREATED ON SITE MENU--------------------------------------------------------------------------+
+
+$eplug_link      = FALSE;
+$eplug_link_name = "";
+$eplug_link_url  = "";
+
+//MESSAGE WHEN PLUGIN IS INSTALLED-------------------------------------------------------------------------+
+
+$eplug_done = $eplug_name." has been sucessfully installed!";
+
+//SAME AS ABOVE BUT ONLY RUN WHEN CHOOSING UPGRADE---------------------------------------------------------+
+
+$upgrade_add_prefs    = $eplug_prefs;
+$upgrade_remove_prefs = "";
+$upgrade_alter_tables = "";
+$eplug_upgrade_done   = "";
+
+//---------------------------------------------------------------------------------------------------------+
+
+?>
